2810fa802dabd8f31138d3b14f0f8f3e687372da
Dhairya Patel <101339040+dhairyapatel1506@users.noreply.github.com>
2024-11-10 23:32:27 +0530

    Changed DELAY value

--- GamePanel.java
+++ GamePanel.java
@@ -9,7 +9,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    final int DELAY = 95;
+    final int DELAY = 90;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
